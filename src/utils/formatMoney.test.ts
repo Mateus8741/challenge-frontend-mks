682cee42318280e3e84c95b8e4e9991cd9bb5cc8
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-24 19:03:02 -0300

    Refactor formatMoney test to use it instead of test in formatMoney.test.ts

--- src/utils/formatMoney.test.ts
+++ src/utils/formatMoney.test.ts
@@ -1,7 +1,7 @@
 import { formatMoney } from './formatMoney'
 
 describe('formatMoney function', () => {
-  test('should format money correctly for integer values', () => {
+  it('should format money correctly for integer values', () => {
     expect(formatMoney(249)).toBe('R$ 249')
     expect(formatMoney(2499)).toBe('R$ 2.499')
     expect(formatMoney(24999)).toBe('R$ 24.999')
