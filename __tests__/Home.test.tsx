cdfa01412c8ac0ad7112570bbc0ea27ea2e41a9f
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-24 18:59:16 -0300

    Add yarn.lock to .gitignore and update Home test in Home.test.tsx

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,3 +1,19 @@
-it('test', () => {
-  expect(1).toBe(1)
+import Home from '@/app/page'
+import { useOpenCart } from '@/contexts/useOpenCart'
+import { render, screen } from '@testing-library/react'
+import { AnimatePresence } from 'framer-motion'
+
+describe('Home component', () => {
+  test('renders Cart when cartOpen is true', () => {
+    const { setCartOpen } = useOpenCart()
+    setCartOpen(true)
+
+    render(
+      <AnimatePresence>
+        <Home />
+      </AnimatePresence>,
+    )
+
+    expect(screen.getByText('Cart')).toBeInTheDocument()
+  })
 })
