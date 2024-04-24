e0da13369726954fc8832215ceec68e4d1e7836f
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-24 14:31:12 -0300

    Update Card component layout and functionality in Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,12 +9,16 @@ type CardProps = HtmlHTMLAttributes<HTMLButtonElement> & {
 
 export function Card({ data, ...props }: CardProps) {
   return (
-    <div className="w-[20rem] h-[285] bg-white rounded-lg shadow-xl">
+    <div className="flex flex-col justify-between items-end w-[20rem] h-[285] bg-white rounded-lg shadow-xl lg:h-[31rem]">
       <div className="flex flex-col p-5">
-        <img src={data.photo} alt="Placeholder" className="rounded-lg" />
+        <img
+          src={data.photo}
+          alt={data.name}
+          className="rounded-lg w-full h-[15rem] object-contain"
+        />
 
         <div className="flex flex-row mt-5 items-center justify-between">
-          <h3 className="font-normal text-xl text-start max-w-36 text-bg-price">
+          <h3 className="font-normal text-lg text-start max-w-40 text-bg-price lg:text-xl">
             {data.name}
           </h3>
           <p className="font-bold text-xl bg-bg-price py-1 px-2.5 rounded-lg text-white">
