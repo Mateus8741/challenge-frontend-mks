cb5b7af615a6ffb835e2ea0179af05005f9e9eb6
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-25 11:55:54 -0300

    Refactor SkeletonLoading component to use gray background color in SkeletonLoading.tsx

--- src/components/SkeletonLoading.tsx
+++ src/components/SkeletonLoading.tsx
@@ -5,17 +5,19 @@ export function SkeletonLoading() {
       data-testid="skeleton-loading"
     >
       <div className=" flex flex-col p-5">
-        <div className="rounded-lg h-48 bg-bg-price" />
+        <div className="rounded-lg h-48 bg-gray-400" />
 
         <div className="flex flex-row mt-5 items-center justify-between">
-          <div className="h-4 bg-bg-price w-1/2" />
-          <div className="h-4 bg-bg-price w-1/4" />
+          <div className="h-4 bg-gray-400 w-1/2" />
+          <div className="h-4 bg-gray-400 w-1/4" />
         </div>
 
-        <div className="bg-bg-price h-4 mt-2" />
+        <div className="bg-gray-400 h-4 mt-2" />
+        <div className="bg-gray-400 h-4 mt-2" />
+        <div className="bg-gray-400 h-4 mt-2" />
       </div>
 
-      <div className="bg-bg-price h-10 rounded-b-lg" />
+      <div className="bg-gray-400 h-10 rounded-b-lg" />
     </div>
   )
 }
